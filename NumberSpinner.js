4dcb674f22c57e1491da5e80f568b3e0b7eadb07
Evans Thesee <EThesee@phtcorp.com>
2016-05-03 14:09:01 -0400

    NumberSpinner.js

---
+++ NumberSpinner.js
@@ -0,0 +1,17 @@
+{ 
+        "id"        : "TRN080_Q1",
+        "IG"        : "PracticeDiary",
+        "IT"        : "TRN080_Q1",
+        "skipIT"    : "TRN080_Q1_SKP",
+        "text"      : "TRN080_Q1_MSG",
+        "className" : "TRN080_Q1",
+        "widget"    : {
+            "id"        : "NUMBER_SPINNER_W_1",
+            "type"      : "NumberSpinner",
+            "className" : "PILLS_NI",
+            "config" : {
+                "min"       : 0,
+                "max"       : 24
+            }
+        }
+},
